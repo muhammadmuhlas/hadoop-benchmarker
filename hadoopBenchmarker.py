90a423b7f8ac65974e537134a01b1ff33a3bf069
Rashid Ali <rali43@bloomberg.com>
2018-03-12 17:18:52 -0400

    added more tests

--- hadoopBenchmarker.py
+++ hadoopBenchmarker.py
@@ -1,6 +1,9 @@
 import argparse
 import ConfigParser
 import subprocess
+import time
+import os
+import traceback
 
 #create a parser which evaluate command line arguments.
 
@@ -14,21 +17,28 @@ def createArgumentParser():
 # in conf directory
 def loadConfiguration(configFile):
     config = ConfigParser.ConfigParser(allow_no_value=True)
-    config.optionxform = str	
+    config.optionxform = str
     config.readfp(open('conf/'+configFile))
     return config
 
 #This function extracts the argument of the command.
 def getToolArguments(config, testName):
     args = []
+    commandArgs =''
     options = config.options(testName)
     for option in options:
         if (option != 'tool') & (option != 'command'):
-            args.append(option)
-            value = config.get(testName, option)
-            if (option.startswith('--')):
+            if option == 'args':
+                commandArgs = config.get(testName, option)
                 continue
-            args.append(value)
+            value = config.get(testName, option)
+            if not value:
+                args.append(option)
+                continue
+            args.append(option+"="+value)
+
+    if commandArgs:
+        args.append(commandArgs)
     return args
 
 #This function validates the commands and tools and raise an error, command
@@ -51,10 +61,23 @@ def constructCommand(config, testName):
     return process
 
 def executeCommand(process):
-    p = subprocess.Popen(process)
+    p = subprocess.Popen(process, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     result = p.communicate()
-    print '####RESULT OF TEST####'	
-    print result
+    return result
+
+def createDirectories(dirs):
+    for directory in dirs:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+def createOpenFile(filepath, fileOp):
+    if not os.path.exists(os.path.dirname(filepath)):
+        os.makedirs(os.path.dirname(filepath))
+    return open(filepath, fileOp)
+
+def log(message):
+    print message
+
 
 def main():
     print 'Starting Tests....'
@@ -64,15 +87,35 @@ def main():
     testRunConfig = loadConfiguration(args.configFile)
 
     testNames = testRunConfig.sections()
+
     print testNames
+
+    FILE_FLAG_CREATE_IF_NOT_EXISTS = "a+"
+    logFileName = "logs/"+args.configFile+str(time.time())+'.out'
+    resultFileName = "results/"+args.configFile+str(time.time())+'_results.out'
+
+    outfile = createOpenFile(logFileName, FILE_FLAG_CREATE_IF_NOT_EXISTS)
+    summary = createOpenFile(resultFileName, FILE_FLAG_CREATE_IF_NOT_EXISTS)
 
     for testName in testNames:
         try:
+            log('running test: '+testName)
             process = constructCommand(testRunConfig, testName)
-            executeCommand(process)
+            result = executeCommand(process)
+            outfile.write("######"+testName+"############\n")
+            summary.write("######"+testName+"############\n")
+            summary.write(str(process))
+            outfile.write(result[0])
+            for line in result[0].splitlines():
+                if 'Summary of timings' in line:
+                    summary.write(line[line.index("Summary of timings"):])
+            log('completed test: '+testName)
         except Exception as exp:
             print 'Test: '+testName +' is failed ', exp
+            traceback.print_exc()
 
 
+    outfile.close()
+    summary.close()
 if __name__ == '__main__':
     main()
