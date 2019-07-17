9fc1ed42f47ecc2510443a4a464406db67b134e8
Muhammad Muhlas <muhammadmuhlas3@gmail.com>
2019-07-18 06:46:00 +0700

    tidy up

--- hadoopBenchmarker.py
+++ hadoopBenchmarker.py
@@ -8,9 +8,9 @@ import json
 import shutil
 import shlex
 
-FILE_FLAG_CREATE_IF_NOT_EXISTS = "a+"
-LOG_DIR = "/tmp/logs"
-RESULT_DIR = "/tmp/results"
+FILE_FLAG_CREATE_IF_NOT_EXISTS = 'a+'
+LOG_DIR = '/tmp/logs'
+RESULT_DIR = '/tmp/results'
 
 EXECUTION_TIME = 'ExecutionTime'
 PARAMS = 'params'
@@ -26,11 +26,14 @@ JAR_CLASS = 'jarClass'
 
 appCfg = None
 
+
 def createArgumentParser():
     usage = './bin/hadoop-benchmarker.sh --config-file <configFileName>'
     parser = argparse.ArgumentParser(usage=usage)
-    parser.add_argument('--config-file' ,'-c' ,dest='configFile', required=True)
+    parser.add_argument('--config-file', '-c', dest='configFile',
+                        required=True)
     return parser
+
 
 def loadConfiguration(configFile):
     config = ConfigParser.ConfigParser(allow_no_value=True)
@@ -38,11 +41,14 @@ def loadConfiguration(configFile):
     config.readfp(open(configFile))
     return config
 
+
 def executeCommand(process):
-    log("Command:"+str(process))
-    p = subprocess.Popen(process, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    log('Command:' + str(process))
+    p = subprocess.Popen(process, stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
     result = p.communicate()
     return result
+
 
 def getTestRunParams(config, testName):
     params = {}
@@ -53,6 +59,7 @@ def getTestRunParams(config, testName):
 
     return params
 
+
 def getTestTerasortArguments(config, testName):
     TERASORTARGS = {
         'JAR_FILE_PATH': 'jarFilePath',
@@ -61,8 +68,8 @@ def getTestTerasortArguments(config, testName):
         'NUMBER_FILES': 'numberFiles',
         'FILE_SIZE': 'fileSize',
         'DIR_INPUT': 'dirInput',
-        'DIR_OUTPUT': 'dirOutput'
-    }
+        'DIR_OUTPUT': 'dirOutput',
+        }
 
     jarFilePath = ''
     jarClass = ''
@@ -92,15 +99,16 @@ def getTestTerasortArguments(config, testName):
 
     return args
 
+
 def getTestDfsioArguments(config, testName):
     DFSIOARGS = {
-    'JAR_FILE_PATH': 'jarFilePath',
-    'JAR_CLASS': 'jarClass',
-    'TEST_TYPE': 'testType',
-    'NUMBER_FILES': 'numberFiles',
-    'FILE_SIZE': 'fileSize',
-    'TEST_PROPERTIES': 'testProperties'
-    }
+        'JAR_FILE_PATH': 'jarFilePath',
+        'JAR_CLASS': 'jarClass',
+        'TEST_TYPE': 'testType',
+        'NUMBER_FILES': 'numberFiles',
+        'FILE_SIZE': 'fileSize',
+        'TEST_PROPERTIES': 'testProperties',
+        }
 
     jarFilePath = ''
     jarClass = ''
@@ -131,12 +139,11 @@ def getTestDfsioArguments(config, testName):
     args.append(jarClass)
 
     props = testProperties.split(',')
-
     for prop in props:
         args.append('-D')
         args.append(prop)
 
-    args.append('-'+testType)
+    args.append('-' + testType)
     args.append('-nrFiles')
     args.append(numberFiles)
     args.append('-fileSize')
@@ -144,20 +151,23 @@ def getTestDfsioArguments(config, testName):
 
     return args
 
+
 def constructCommand(config, testName):
     process = []
     allowedCommands = [HADOOP]
     allowedTools = ['jar']
     command = config.get(testName, COMMAND)
     if command not in allowedCommands:
-        raise Exception('command: '+command + ' is not supported')
+        raise Exception('command: ' + command + ' is not supported')
     tool = config.get(testName, 'tool')
     if tool not in allowedTools:
-        raise Exception('tool: '+tool + ' is not supported')
+        raise Exception('tool: ' + tool + ' is not supported')
 
-    if command == HADOOP and config.get(testName, JAR_CLASS) == 'TestDFSIO':
+    if command == HADOOP and config.get(testName, JAR_CLASS) \
+        == 'TestDFSIO':
         process = getTestDfsioArguments(config, testName)
-    elif command == HADOOP and config.get(testName, JAR_CLASS) == 'Terasort':
+    elif command == HADOOP and config.get(testName, JAR_CLASS) \
+        == 'Terasort':
         process = getTestTerasortArguments(config, testName)
 
     return process
@@ -168,27 +178,31 @@ def createDirectories(dirs):
         if not os.path.exists(directory):
             os.makedirs(directory)
 
+
 def createOpenFile(filepath, fileOp):
     if not os.path.exists(os.path.dirname(filepath)):
         os.makedirs(os.path.dirname(filepath))
     return open(filepath, fileOp)
 
+
 def log(message):
     print message
 
+
 def addResult(results, key, line):
-    data = line[line.index(key):].rsplit(":",1)
+    data = line[line.index(key):].rsplit(':', 1)
     key = data[0].strip()
     value = data[1].strip()
     results[key] = value
 
+
 def extractDFSIOResults(lines):
     dfsio_results = {}
-    TOTAL_MBYTES_PROCESSED_KEY = "Total MBytes processed"
-    THROUGHPUT_KEY = "Throughput mb/sec"
-    AVERAGE_IO_KEY = "Average IO rate mb/sec"
-    IO_RATE_STD_DEVIATION_KEY = "IO rate std deviation"
-    TEST_EXEC_TIME_KEY = "Test exec time sec"
+    TOTAL_MBYTES_PROCESSED_KEY = 'Total MBytes processed'
+    THROUGHPUT_KEY = 'Throughput mb/sec'
+    AVERAGE_IO_KEY = 'Average IO rate mb/sec'
+    IO_RATE_STD_DEVIATION_KEY = 'IO rate std deviation'
+    TEST_EXEC_TIME_KEY = 'Test exec time sec'
 
     for line in lines:
         if TOTAL_MBYTES_PROCESSED_KEY in line:
@@ -204,9 +218,10 @@ def extractDFSIOResults(lines):
 
     return dfsio_results
 
+
 def extractTerasortResults(lines):
     terasort_results = {}
-    TEST_EXEC_TIME_KEY = "Total time spent by all map tasks (ms)"
+    TEST_EXEC_TIME_KEY = 'Total time spent by all map tasks (ms)'
 
     for line in lines:
         if TEST_EXEC_TIME_KEY in line:
@@ -214,13 +229,16 @@ def extractTerasortResults(lines):
 
     return terasort_results
 
+
 def copyHadoopConfigFiles(targetDir):
-    configFiles = ['/usr/local/hadoop/etc/hadoop/core-site.xml', '/usr/local/hadoop/etc/hadoop/hdfs-site.xml']
-    confDir = os.path.join(targetDir, "conf")
+    configFiles = ['/usr/local/hadoop/etc/hadoop/core-site.xml',
+                   '/usr/local/hadoop/etc/hadoop/hdfs-site.xml']
+    confDir = os.path.join(targetDir, 'conf')
     createDirectories([confDir])
 
     for file in configFiles:
         shutil.copy(file, confDir)
+
 
 def copyResultsToHdfs(testResultDir):
     command = []
@@ -230,75 +248,82 @@ def copyResultsToHdfs(testResultDir):
     command.append('-f')
     command.append(testResultDir)
     print type(appCfg)
-    command.append(appCfg.get(APP_CONFIG,'hdfs_location'))
+    command.append(appCfg.get(APP_CONFIG, 'hdfs_location'))
 
     executeCommand(command)
+
 
 def saveServerSideConfig(resultDir, rsUrl):
     outputFile = os.path.join(resultDir, 'resourceManagerConfig.xml')
 
-    command = "curl --negotiate -o "+ outputFile + " " + rsUrl
+    command = 'curl --negotiate -o ' + outputFile + ' ' + rsUrl
 
     executeCommand(shlex.split(command))
+
 
 def main():
     log('Starting Tests....')
     argParser = createArgumentParser()
     args = argParser.parse_args()
-    log('configFile='+args.configFile)
+    log('configFile=' + args.configFile)
     testRunConfig = loadConfiguration(args.configFile)
     global appCfg
-    appCfg = loadConfiguration(os.path.join('conf','application.cfg'))
+    appCfg = loadConfiguration(os.path.join('conf', 'application.cfg'))
     testNames = testRunConfig.sections()
 
     log(testNames)
 
     configFileName = os.path.split(args.configFile)[1]
-    testRunId = configFileName+time.strftime("%Y%m%dT%H%M%S", time.localtime())
-    testResultDir = os.path.join(RESULT_DIR,testRunId)
+    testRunId = configFileName + time.strftime('%Y%m%dT%H%M%S',
+            time.localtime())
+    testResultDir = os.path.join(RESULT_DIR, testRunId)
 
-    logFileName = os.path.join(LOG_DIR,testRunId+'.out')
+    logFileName = os.path.join(LOG_DIR, testRunId + '.out')
     resultFileName = os.path.join(testResultDir, 'result.json')
 
-    outfile = createOpenFile(logFileName, FILE_FLAG_CREATE_IF_NOT_EXISTS)
-    resultsFile = createOpenFile(resultFileName, FILE_FLAG_CREATE_IF_NOT_EXISTS)
+    outfile = createOpenFile(logFileName,
+                             FILE_FLAG_CREATE_IF_NOT_EXISTS)
+    resultsFile = createOpenFile(resultFileName,
+                                 FILE_FLAG_CREATE_IF_NOT_EXISTS)
 
     command = testRunConfig.defaults().get('command')
 
     results = []
     for testName in testNames:
         try:
-            log('running test: '+testName)
-            executionTime = time.strftime("%Y-%m-%dT%H:%M:%S")
+            log('running test: ' + testName)
+            executionTime = time.strftime('%Y-%m-%dT%H:%M:%S')
 
             process = constructCommand(testRunConfig, testName)
             log('following process will run' + str(process))
             result = executeCommand(process)
             log('test executed, parsing starts: ' + testName)
 
-            outfile.write("###"+testName+"###\n")
+            outfile.write('###' + testName + '###\n')
             outfile.write(result[0])
             lines = iter(result[0].splitlines())
 
             testResult = {}
             extractedResults = {}
             command = testRunConfig.get(testName, COMMAND)
-            if command == HADOOP and testRunConfig.get(testName, JAR_CLASS) == 'TestDFSIO':
+            if command == HADOOP and testRunConfig.get(testName,
+                    JAR_CLASS) == 'TestDFSIO':
                 extractedResults = extractDFSIOResults(lines)
-            elif command == HADOOP and testRunConfig.get(testName, JAR_CLASS) == 'Terasort':
+            elif command == HADOOP and testRunConfig.get(testName,
+                    JAR_CLASS) == 'Terasort':
                 extractedResults = extractTerasortResults(lines)
-
 
             testResult.update(extractedResults)
             testResult[EXECUTION_TIME] = executionTime
             testResult[NAME] = testName
-            testResult[PARAMS] = getTestRunParams(testRunConfig, testName)
+            testResult[PARAMS] = getTestRunParams(testRunConfig,
+                    testName)
 
             results.append(testResult)
-            log('test completed:'+testName)
-        except Exception as exp:
-            results.append("ERROR: check log files")
-            print 'Test: '+testName +' is failed ', exp
+            log('test completed:' + testName)
+        except Exception, exp:
+            results.append('ERROR: check log files')
+            print 'Test: ' + testName + ' is failed ', exp
             traceback.print_exc()
 
     json.dump(results, resultsFile)
@@ -307,6 +332,7 @@ def main():
 
     copyHadoopConfigFiles(testResultDir)
     copyResultsToHdfs(testResultDir)
+
 
 if __name__ == '__main__':
     main()
